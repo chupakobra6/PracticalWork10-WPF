a94ff6bbaf7e36e6fdb7596c6fc3ff0017171864
chupacobra6 <igorpheik@gmail.com>
2023-06-09 21:55:36 +0300

    Добавьте файлы проекта.

---
+++ SunClouds/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PracticalWork8.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunClouds.ViewModel
+{
+    internal class MainViewModel : BindingHelper // Для команд
+    {
+        #region Properties (свойства) // Регионы для читабельности кода
+        #endregion
+        #region Commands (команды)
+        #endregion
+        #region Events (события)
+        #endregion
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
