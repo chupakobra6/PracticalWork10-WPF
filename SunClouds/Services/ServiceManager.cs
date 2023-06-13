23e6c8d135be3e400ab6e4183db9ee558970ae84
Игорь <112656648+chupakobra6@users.noreply.github.com>
2023-06-13 12:25:26 +0300

    Revert "Остальные файлы API"

--- SunClouds/Services/ServiceManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WeatherApp.Services
-{
-	internal static class ServiceManager
-    {
-		private readonly static InternetConnectionService _connectionService = new InternetConnectionService();
-
-		public static InternetConnectionService InternetConnectionService => _connectionService;
-    }
-}
